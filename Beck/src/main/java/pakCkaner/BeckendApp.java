3318a4ab06d517c1fe41679c9731843c67bcbc23
alex.branavitckiy@gmail.com <alex.branavitckiy@gmail.com>
2019-11-07 22:48:46 +0300

    add strakt

---
+++ Beck/src/main/java/pakCkaner/BeckendApp.java
@@ -0,0 +1,13 @@
+package pakCkaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BeckendApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeckendApp.class, args);
+    }
+}
